80289491f2230ee704e23037f88f97761b7a4ce5
JSchoonveld <jesse.schoonveld@gmail.com>
2022-05-17 07:40:27 +0200

    strapi added partially

--- next.config.js
+++ next.config.js
@@ -3,4 +3,13 @@ const nextConfig = {
   reactStrictMode: true,
 }
 
-module.exports = nextConfig
+module.exports = {
+    reactStrictMode: true,
+    images: {
+        loader: "default",
+        domains: ["localhost"],
+    },
+    nextConfig
+};
+
+// module.exports = nextConfig
