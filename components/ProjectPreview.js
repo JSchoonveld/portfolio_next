5e47be2e35f16bc71b320cfc01dbc1245a10b4cd
JSchoonveld <jesse.schoonveld@gmail.com>
2022-05-19 22:13:27 +0200

    Project previews animated on hover

--- components/ProjectPreview.js
+++ components/ProjectPreview.js
@@ -1,22 +1,56 @@
 import {Image} from "react-bootstrap";
+import {useState} from "react";
+import {animated, useSpring} from "react-spring";
+import { Waypoint } from 'react-waypoint';
+
 
 export default function ProjectPreview(props) {
+    const [showDetails, setShowDetails] = useState(false);
+    const [inView, setInView] = useState(false);
+
+    const transitionUp = useSpring({
+        delay: 100,
+        to: {opacity: !inView ? '0' : '1', bottom: !inView ? '0px' : '20px'},
+    });
+
+    const transitionDown = useSpring({
+        delay: 100,
+        to: {opacity: !inView ? '0' : '1', top: !inView ? '0px' : '20px'},
+    });
+
+    let showButton = (() => {
+
+        return (
+            <div onMouseEnter={() => setInView(true)} onMouseLeave={() => setInView(false)} className={"project-preview-overlay"}>
+                <div className="content">
+                    <animated.div style={{ position: 'relative', ...transitionDown }}>
+                        <h3>{props.title}</h3>
+                        <p>{props.techs}</p>
+                    </animated.div>
+                    <animated.a style={{ position: 'relative', ...transitionUp }} className={"btn btn-primary"} href={props.website} target={"_blank"} rel="noopener noreferrer">Visit</animated.a>
+                </div>
+            </div>
+
+        )
+    })
 
     return (
-            <a href={props.website} target={"_blank"} rel="noopener noreferrer">
-                <div className="project">
+        // <a href={props.website} target={"_blank"} rel="noopener noreferrer">
+        <div onMouseEnter={() => setShowDetails(true)} onMouseLeave={() => setShowDetails(false)}  style={{position: 'relative'}} className="project">
+            {showDetails ? showButton() : ''}
+            {/*<h3 className={"mb-3 text-center"}>{props.title}</h3>*/}
+            <Image
+                src={props.imageUrl}
+                alt={props.title}
+                width={335}
+                height={210}
+            />
 
-                    <h3 className={"mb-3"}>{props.title}</h3>
-                    <Image
-                        src={props.imageUrl}
-                        alt={props.title}
-                        width={335}
-                        height={210}
-                    />
-
-                </div>
-            </a>
+        </div>
+        // </a>
 
 
     )
+
+
 }
---
+++ components/QualitySection.js
@@ -0,0 +1,20 @@
+import Quality from "./Quality";
+
+export default function QualitySection() {
+    return (
+        <div className="row mt-5 mb-0">
+            <div className="col-6 col-lg-3 d-flex justify-content-center text-center mb-3 mb-lg-5 mb-lg-0">
+                <Quality delay={200} title={"Fast"} description={"My websites are lightning fast"} imageUrl={"/icons/speedometer.png"}/>
+            </div>
+            <div className="col-6 col-lg-3 d-flex justify-content-center text-center mb-3 mb-lg-5 mb-lg-0">
+                <Quality delay={300} title={"Intuitive"} description={"A modern website should be easy to use"} imageUrl={"/icons/intuitive.png"} />
+            </div>
+            <div className="col-6 col-lg-3 d-flex justify-content-center text-center mb-3 mb-lg-5 mb-lg-0">
+                <Quality delay={400} title={"Responsive"} description={"Looks great on all devices and screen sizes"} imageUrl={"/icons/responsive.png"} />
+            </div>
+            <div className="col-6 col-lg-3 d-flex justify-content-center text-center mb-3 mb-lg-5 mb-lg-0">
+                <Quality delay={500} title={"Seo friendly"} description={"I combine server side and client side rendering for the best of both worlds"} imageUrl={"/icons/seo.png"} />
+            </div>
+        </div>
+    )
+}
