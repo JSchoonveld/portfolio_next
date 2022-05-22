95c70c56d95205e65dfd4e2e6fab5f7fb08f6055
JSchoonveld <jesse.schoonveld@gmail.com>
2022-05-22 11:54:05 +0200

    Switched Strapi for Storyblok

---
+++ components/Preview.js
@@ -0,0 +1,119 @@
+import { storyblokEditable } from "@storyblok/react";
+import {Button, Image, Modal, Carousel, CarouselItem} from "react-bootstrap";
+import {useState} from "react";
+import {animated, useSpring} from "react-spring";
+
+const Preview = ({ blok }) => {
+    const [showDetails, setShowDetails] = useState(false);
+    const [inView, setInView] = useState(false);
+
+    const [show, setShow] = useState(false);
+
+    const handleClose = (()=> {
+        setShow(false)
+        setShowDetails(false)
+    })
+    const handleShow = (()=> {
+        setShow(true)
+    })
+
+
+    const transitionUp = useSpring({
+        delay: 100,
+        to: {opacity: !inView ? '0' : '1', bottom: !inView ? '0px' : '20px'},
+    });
+
+    const transitionDown = useSpring({
+        delay: 100,
+        to: {opacity: !inView ? '0' : '1', top: !inView ? '0px' : '20px'},
+    });
+
+    let showButton = (() => {
+
+        return (
+            <div onMouseOver={() => setInView(true)} onMouseOut={() => setInView(false)}  className={"project-preview-overlay"}>
+                <div className="content">
+                    <animated.div style={{ position: 'relative', ...transitionDown }}>
+                        <h3>{blok.title}</h3>
+                        <p>{blok.techs}</p>
+                    </animated.div>
+
+                    <animated.div style={{ position: 'relative', ...transitionUp }}>
+                        <Button variant="primary" onClick={handleShow}>
+                            Read more
+                        </Button>
+                    </animated.div>
+
+
+                    <Modal show={show} onHide={handleClose}>
+                        <Modal.Header>
+                            {/*<Carousel indicators={false}>*/}
+                            {/*    <Carousel.Item>*/}
+                            {/*        <Image*/}
+                            {/*            src={blok.images[0].filename}*/}
+                            {/*            alt={blok.title}*/}
+                            {/*            width={335}*/}
+                            {/*            height={210}*/}
+                            {/*        />*/}
+
+                            {/*    </Carousel.Item>*/}
+                            {/*    <Carousel.Item>*/}
+                            {/*        <Image*/}
+                            {/*            src={blok.images[1].filename}*/}
+                            {/*            alt={blok.title}*/}
+                            {/*            width={335}*/}
+                            {/*            height={210}*/}
+                            {/*        />*/}
+
+                            {/*    </Carousel.Item>*/}
+                            {/*    <Carousel.Item>*/}
+                            {/*        <Image*/}
+                            {/*            src={blok.images[2].filename}*/}
+                            {/*            alt={blok.title}*/}
+                            {/*            width={335}*/}
+                            {/*            height={210}*/}
+                            {/*        />*/}
+
+                            {/*    </Carousel.Item>*/}
+                            {/*</Carousel>*/}
+
+                        </Modal.Header>
+                        <Modal.Body>
+                            <Modal.Title><b>{blok.title}</b></Modal.Title>
+                            <p><b>Techstack:</b> {blok.techs}</p>
+
+                            {blok.content}
+                        </Modal.Body>
+                        <Modal.Footer>
+                            <div>
+                                <Button variant="secondary" onClick={handleClose}>
+                                    Close
+                                </Button>
+                            </div>
+                            <div>
+                                <a className={"btn btn-primary"} href={blok.website} target={"_blank"} rel="noopener noreferrer">Visit website</a>
+                            </div>
+                        </Modal.Footer>
+                    </Modal>
+                </div>
+            </div>
+
+        )
+    })
+
+
+    return (
+        <div tabIndex={0} onMouseOver={() => setShowDetails(true)} onMouseLeave={() => setShowDetails(false)} style={{position: 'relative'}} className="project col-md-4">
+            {showDetails ? showButton() : ''}
+            <Image
+                src={blok.thumbnail.filename}
+                alt={blok.title}
+                width={335}
+                height={210}
+            />
+
+        </div>
+    )
+};
+
+export default Preview;
--- pages/_app.js
+++ pages/_app.js
@@ -2,9 +2,32 @@ import 'bootstrap/dist/css/bootstrap.css'
 import '../styles/globals.scss'
 import "@fortawesome/fontawesome-svg-core/styles.css"; // import Font Awesome CSS
 import { config } from "@fortawesome/fontawesome-svg-core";
+import Feature from "../components/Feature";
+import Grid from "../components/Grid";
+import Page from "../components/Page";
+import Teaser from "../components/Teaser";
+// import _ProjectPreview from "../components/ProjectPreview";
+import Preview from "../components/Preview";
+import Projects from '../components/Projects'
 import { useSpring, animated } from 'react-spring'
 config.autoAddCss = false; // Tell Font Awesome to skip adding the CSS
 
+const components = {
+    feature: Feature,
+    grid: Grid,
+    teaser: Teaser,
+    page: Page,
+    preview: Preview,
+    projects: Projects,
+};
+
+import { storyblokInit, apiPlugin } from "@storyblok/react";
+
+storyblokInit({
+    accessToken: "TrvOg6PotJmshV7jFuGFqgtt",
+    use: [apiPlugin],
+    components,
+});
 
 function MyApp({Component, pageProps}) {
     return <div>
