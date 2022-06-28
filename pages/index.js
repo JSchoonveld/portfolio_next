d5bd05224e05d544faebffeb65bf77b342487179
JSchoonveld <jesse.schoonveld@gmail.com>
2022-06-28 11:15:05 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -27,19 +27,12 @@ export default function Home(props) {
 
     return (
         <div className={styles.container}>
-            <Head>
-    <!-- Google Tag Manager -->
-<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-NWLRDST');</script>
-<!-- End Google Tag Manager -->
+          <Head>
                 <title>Jesse Schoonveld portfolio</title>
                 <meta name="description"
                       content="I'm currently studying web development and doing and internship as a front-end developer. This is my portfolio."/>
                 <link rel="icon" href="favicon.ico"/>
-            </Head>
+           </Head>
             
             <main className={styles.main}>
                 <FadeHeader/>
