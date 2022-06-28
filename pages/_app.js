f0fab19d650265c38f2ccb51f4f4c2c73a014149
Jesse <jesse.schoonveld@gmail.com>
2022-06-28 12:02:13 +0200

    added gtag manager

--- pages/_app.js
+++ pages/_app.js
@@ -1,42 +1,42 @@
-import 'bootstrap/dist/css/bootstrap.css'
-import '../styles/globals.scss'
+import "bootstrap/dist/css/bootstrap.css";
+import "../styles/globals.scss";
 import "@fortawesome/fontawesome-svg-core/styles.css"; // import Font Awesome CSS
 import { config } from "@fortawesome/fontawesome-svg-core";
 import Feature from "../components/Feature";
 import Grid from "../components/Grid";
 import Page from "../components/Page";
 import Teaser from "../components/Teaser";
-// import _ProjectPreview from "../components/ProjectPreview";
+import { useEffect } from "react";
+import TagManager from "react-gtm-module";
 import Preview from "../components/Preview";
-import Projects from '../components/Projects'
-import { useSpring, animated } from 'react-spring'
+import Projects from "../components/Projects";
+import { useSpring, animated } from "react-spring";
 config.autoAddCss = false; // Tell Font Awesome to skip adding the CSS
 
-require('dotenv').config()
+require("dotenv").config();
 
 const components = {
-    feature: Feature,
-    grid: Grid,
-    teaser: Teaser,
-    page: Page,
-    preview: Preview,
-    projects: Projects,
+  feature: Feature,
+  grid: Grid,
+  teaser: Teaser,
+  page: Page,
+  preview: Preview,
+  projects: Projects,
 };
 
 import { storyblokInit, apiPlugin } from "@storyblok/react";
 
 storyblokInit({
-    accessToken: process.env.STORYBLOK_KEY,
-    use: [apiPlugin],
-    components,
+  accessToken: process.env.STORYBLOK_KEY,
+  use: [apiPlugin],
+  components,
 });
 
-function MyApp({Component, pageProps}) {
-    return <div>
-        <Component {...pageProps} />
-    </div>
-
+function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    TagManager.initialize({ gtmId: "GTM-NWLRDST" });
+  }, []);
+  return <Component {...pageProps} />;
 }
 
-
-export default MyApp
+export default MyApp;
